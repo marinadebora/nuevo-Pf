c70cde8320f95c2b85fb9494c48886902ced37ef
Debora <m_deby_c@hotmail.com>
2022-07-29 15:51:58 -0300

    cambios para deploy

--- server/src/dataBase.js
+++ server/src/dataBase.js
@@ -1,15 +1,13 @@
 
 const mongoose = require("mongoose");
 const config = require("./config");
-const {
-    MONGO_USER, MONGO_PASSWORD,MONGO_DATABASE
-  } = process.env;
+const { MONGO_USERS } = process.env;
 
 // conectamos nuestra base de datos
 (async()=>{
     try {
         
-         const db = await mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.mxvbffi.mongodb.net/${MONGO_DATABASE}`);
+         const db = await mongoose.connect(MONGO_USERS);
     console.log('conectada la db', db.connection.name)
     } catch (error) {
         console.error(error)
