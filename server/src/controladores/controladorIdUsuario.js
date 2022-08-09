91a7bfc140002e7d2e8323b9ac95fc8d3bf8b38c
Alexisrafael <alexis24450@gmail.com>
2022-08-09 14:24:50 -0500

    cambio id

--- server/src/controladores/controladorIdUsuario.js
+++ server/src/controladores/controladorIdUsuario.js
@@ -3,7 +3,13 @@ const Usuarios = require("../modelos/Usuarios");
 const UsuarioId= async(req,res)=>{
 const {id}=req.params;
 try {
-    const usuario= await Usuarios.findById(id)
+    const usuario= await Usuarios.findById(id).populate({ 
+        path:"historialDeCompra", 
+        populate: { 
+            path: 'productos.producto', 
+            select: ['producto','imagenes','precio','dimensiones']
+        }
+    })
     res.send(usuario);
 } catch (error) {
     res.status(400).send("Usuario NO encontrado");
